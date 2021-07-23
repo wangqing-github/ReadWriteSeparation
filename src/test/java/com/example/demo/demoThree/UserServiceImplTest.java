7d7f15c72cea855a41f3838acf7e9b12b0dbc427
xiaoyu <be81acfeac5b48ea9bc445727f3ec833>
2021-07-23 10:21:34 +0800

    代码修改

--- src/test/java/com/example/demo/demoThree/UserServiceImplTest.java
+++ src/test/java/com/example/demo/demoThree/UserServiceImplTest.java
@@ -3,6 +3,7 @@ package com.example.demo.demoThree;
 import com.example.demo.DemoApplication;
 import com.example.demo.entity.UserInfo;
 import com.example.demo.service.UserInfoService;
+import com.example.demo.utils.Builder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,10 +27,13 @@ public class UserServiceImplTest {
     @Test
     @Transactional
     public void insert() {
-        UserInfo user = new UserInfo();
-        user.setLoginPassword("qwer");
-        user.setUserName("qwer");
-        user.setId(3);
+        //每天一个装杯小技巧
+        UserInfo user = Builder.of(UserInfo::new).
+                with(UserInfo::setId,1).
+                with(UserInfo::setLoginPassword,"111").
+                with(UserInfo::setUserName,"111").
+                with(UserInfo::setId,3).
+                with(UserInfo::setUserName,"小王").build();
         userService.insert(user);
         int a = 1/0;
     }
